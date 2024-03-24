154c138bf968bb06d9e39ee19009d5f9c9de4c4d
juan-lien-do <findmejuan@hotmail.com>
2024-03-24 18:18:23 -0300

    equiparar altura de tarjetas

--- src/components/Resena.jsx
+++ src/components/Resena.jsx
@@ -5,7 +5,7 @@ import Card from "react-bootstrap/Card";
 export default function Resena({ title, description }) {
   return (
     <div className="card-container">
-      <Card className="card-figure">
+      <Card className="card-figure my-2 h-100 ">
         <Card.Body>
           <Card.Title style={{ color: "black" }}>{title}</Card.Title>
           <Card.Text style={{ color: "white" }}>{description}</Card.Text>
